28b491125639f77a679e296311b110cd1733db69
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-06-08 11:02:00 +0200

    Add files via upload

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,8 +1,9 @@
 document.write(
 "<aside class='topNav nav-blue' id='topNav'>" +
 		"<div class='container navBar'>" +
-			"<a href='https://beangreen247.github.io/'><i class='fa fa-home fa-fw fa-2x'></i></a>" +
-			"<a target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'><i class='fa fa-youtube-play fa-fw fa-2x'></i></a>"+
+			"<a title='Home' href='https://beangreen247.github.io/'><i class='fa fa-home fa-fw fa-2x'></i></a>" +
+			"<a title='Main channel' target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'><i class='fa fa-youtube-play fa-fw fa-2x'></i></a>"+
+			"<a title='Music channel' target='_blank'href='https://www.youtube.com/channel/UC_Qo-mebrSPXU7EpkyJfJ0g'><i class='fa fa-youtube-play fa-fw fa-2x'></i></a>"+
 		"</div>" +
 	"</aside>"
 );
